efe93437f68ff10a8c8767de94409ef85b2e4d48
MiDew## <97555748+Micelloplutarh@users.noreply.github.com>
2023-03-25 16:19:40 +0300

    commit message

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const anim = lottie
+
+anim.loadAnimation({
+    container: document.querySelector('#lottie-test'),
+    renderer: 'svg',
+    loop: true ,
+    autplay: true,
+    patch: './5HeDmnHWUF.json'
+});
